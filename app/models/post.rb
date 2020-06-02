a26e021df73b44193c767dfc329ed3cb77fbc107
shogo <shogo9400@gmail.com>
2020-06-02 21:32:14 +0900

    投稿機能を修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,8 @@ class Post < ApplicationRecord
   has_many :comments
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 100 }
+  validates :title, presence: { message: "動画が存在しません" }, length: { maximum: 100 }
+  validates :youtube_url, presence: { message: "動画URLを入力してください" }, length: { maximum: 100 }
 
   def self.search(search)
     if search
