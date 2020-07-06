922ebc007b7398cf9e4f5688915304f84f027ade
shogo <shogo9400@gmail.com>
2020-07-06 19:50:50 +0900

    レイアウト修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,24 +34,28 @@ title_and_url = {
   "2012MLB10　ダルビッシュVSイチロー2度目の対決" => "e57zbXC4Y5k",
   "「常識人間」は成功しない" => "L99WnfYCeXg",
   "嵐 - Monster [Official Music Video]" => "JedKBJVHhiE",
-  %Q[NBA "Don't Reach" MOMENTS] => "ZPX4hTEdQaw"
+  %Q[NBA "Don't Reach" MOMENTS] => "ZPX4hTEdQaw",
+  "the Fourth Avenue Cafe" => "xSWFEGVjmQ8",
+  "Super Mario Beatbox" => "LE-JN7_rxtE",
+  "【好きなことで、生きていく】『レペゼン地球-DJ社長-』" => "PPnbEiXSYM8",
+  "当たりはなかった？祭りくじで悪事を働く一部始終をban覚悟で完全公開します" => "30roBDlCA7k",
+  "【修羅場】お店の客が全員ラファエルの元カノドッキリ【ラファエル】"=> "zLXZpFftHVk",
+  "たくさんの男性YouTuberで恋ダンス踊ってみた。" => "V502qWGmLFk",
+  "【シバター監修MV】俺たち金持ちYouTuber/カルxピン（11thシングル）" => "fytyMTaF1x4",
+  "GLAY / HOWEVER" => "gPcPseeICjs",
+  "TVアニメ「進撃の巨人」The Final Season　PV" => "SlNpRThS9t8",
+  "リヴァイ VS 獣の巨人(ジーク) 進撃の巨人 Season3 Part2" => "UPKs2ZuQ3ac",
+  "【公式】テニスの王子様 第65話「青学の柱になれ」" => "Llhu-L9r-8k",
+  "One Love" => "fk2URdKTrmQ",
+  "友達の家の壁1面をポケモンカードしてみた【ドッキリ】" => "sy-RzyvMkLg",
+  "残り0.9秒！高校バスケ・小学生ミニバスで起きた奇跡のブザービーター 終了間際の大逆転が感動的すぎる！！！Twitterで話題沸騰中！！" => "1QYby70Trqo",
+  "Kei Nishikori vs Pablo Carreno Busta - Full Fifth Set Tiebreak | Australian Open 2019 4R" => "zNZ9EtnceqA",
+  "バスケを知らない人に見せたいダンク" => "SsmfYJEamyU"
 }
+
 title_and_url.each do |key, value|
   User.first.posts.create!(title: key, youtube_url: value)
 end
-# @post = User.first.posts.create!(title: 'Starbucks Music: 3 Hours of Happy Starbucks Music with Starbucks Music Playlist Youtube',
-#                         youtube_url: 'rMurJ6SxuHs')
-#
-# @post2 = User.first.posts.create!(title: 'Home Bossa: Happy Bossa Nova & Morning Jazz Coffee Music for Good Mood and Wake Up',
-#                         youtube_url: '3N0K0_9cvcg')
-#
-# @post3 = User.first.posts.create!(title: '2012MLB10　ダルビッシュVSイチロー2度目の対決',
-#                         youtube_url: 'e57zbXC4Y5k')
-#
-#
-# @post4 = User.second.posts.create!(title: '「常識人間」は成功しない',
-#                         youtube_url: 'L99WnfYCeXg')
-
 
 # リレーションシップ
 users = User.all
