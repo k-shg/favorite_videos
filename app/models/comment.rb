5fc6da75839bdbda1946eba6f23fe97ae3d809c8
shogo <shogo9400@gmail.com>
2020-07-07 19:09:26 +0900

    レビュー上限文字数を変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :content, presence: true, length: { maximum: 300 }
+  validates :content, presence: true, length: { maximum: 500 }
 end
