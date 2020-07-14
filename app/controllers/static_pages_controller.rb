a7cbbd67bb7579fb2f192f28f6fcfc5fba79a339
shogo <shogo9400@gmail.com>
2020-07-14 22:52:30 +0900

    レイアウト修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @user = current_user
     @feed_items = Post.paginate(page: params[:page], per_page: 15)
     render :layout => 'home'
   end
