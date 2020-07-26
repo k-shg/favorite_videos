ec09dd7f857a4826e8d9f151f767338014453c84
shogo <shogo9400@gmail.com>
2020-07-26 16:28:33 +0900

    レイアウト変更

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
@@ -5,7 +7,6 @@ RSpec.describe PostsController, type: :controller do
   let(:user) { FactoryBot.create(:taro) }
 
   describe 'GET #index' do
-
     before do
       get :show, params: { id: posted.id }
     end
@@ -31,81 +32,77 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe 'POST #create' do
-
     context 'ユーザーがログインしている場合' do
       before { log_in(user) }
       context '正しい投稿情報が渡ってきた場合' do
-
         let(:params) do
           {
             post: {
-              title: "バスケを知らない人に見せたいダンク",
-              youtube_url: "SsmfYJEamyU",
+              title: 'バスケを知らない人に見せたいダンク',
+              youtube_url: 'SsmfYJEamyU',
               category_id: 2
             }
           }
         end
 
         it '投稿が一つ増えていること' do
-          expect {
+          expect do
             post :create, params: params
-          }.to change(Post, :count).by(1)
+          end.to change(Post, :count).by(1)
         end
 
-        it 'ルートページにリダイレクトされること' do
+        it 'ルートページにリダイレクトされ���こと' do
           expect(
-            post :create, params: params
+            post(:create, params: params)
           ).to redirect_to root_url
         end
       end
 
       context '不正な投稿情報が渡ってきた場合' do
-
         let(:params) do
           {
             post: {
-              title: "sampletitle",
-              youtube_url: "",
+              title: 'sampletitle',
+              youtube_url: '',
               category_id: 2
             }
           }
         end
 
         it '投稿数が変わらないこと' do
-          expect {
+          expect do
             post :create, params: params
-           }.to change(Post, :count).by(0)
+          end.to change(Post, :count).by(0)
         end
 
         it 'マイページにリダイレクトされること' do
           expect(
-            post :create, params: params
+            post(:create, params: params)
           ).to redirect_to user_path(user)
         end
       end
     end
 
     context 'ユーザーがログインしていない場合' do
-
       let(:params) do
         {
           post: {
-            title: "バスケを知らない人に見せたいダンク",
-            youtube_url: "SsmfYJEamyU",
+            title: 'バスケを知らない人に見せたいダンク',
+            youtube_url: 'SsmfYJEamyU',
             category_id: 2
           }
         }
       end
 
       it '投稿数が変わらないこと' do
-        expect {
+        expect do
           post :create, params: params
-        }.to change(Post, :count).by(0)
+        end.to change(Post, :count).by(0)
       end
 
       it 'ログインページにリダイレクトされること' do
         expect(
-          post :create, params: params
+          post(:create, params: params)
         ).to redirect_to login_path
       end
     end
