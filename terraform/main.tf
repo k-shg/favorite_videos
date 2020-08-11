05f25341dd05b13e76a6a39c36a63a2df29e7bc1
shogo <shogo9400@gmail.com>
2020-08-11 16:03:54 +0900

    バックエンドをs3に設定

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,12 @@ provider "aws" {
 resource "aws_s3_bucket" "tfstate" {
   bucket = "bkt-terraform"
 }
+
+terraform {
+  backend "s3" {
+    region  = "ap-northeast-1"
+    bucket = "bkt-terraform"
+    key = "terraform.tfstate"
+    profile = "my-profile"
+  }
+}
